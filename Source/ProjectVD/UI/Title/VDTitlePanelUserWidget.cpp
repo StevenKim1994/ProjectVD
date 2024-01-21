9e462b8621b77e4d8f2341e9a965cfaa59740b9c
김시윤 <enqueuedequeue@naver.com>
2024-01-21 12:58:53 +0900

    * Stage Level 추가 및 UI내에서 게임종료 Level 이동 이벤트 바인딩

--- Source/ProjectVD/UI/Title/VDTitlePanelUserWidget.cpp
+++ Source/ProjectVD/UI/Title/VDTitlePanelUserWidget.cpp
@@ -1,15 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UI/Title/VDTitlePanelUserWidget.h"
+#include "Kismet/GameplayStatics.h"
 
 void UVDTitlePanelUserWidget::OnClickStartButton()
 {
-	UE_LOG(LogTemp, Log, TEXT("StartClick!"));
+	UGameplayStatics::OpenLevel(this, TEXT("Stage"));
 }
 
 void UVDTitlePanelUserWidget::OnClickExitButton()
 {
 	UE_LOG(LogTemp, Log, TEXT("ExitClick!"));
+	UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController() , EQuitPreference::Quit, false);
 }
 
 void UVDTitlePanelUserWidget::NativeConstruct()
@@ -41,7 +43,7 @@ void UVDTitlePanelUserWidget::NativeConstruct()
 		{
 			ExitButtonText->SetText(FText::FromString(FString(TEXT("Exit"))));
 		}
-
+		
 		ExitButtonWidget->OnClicked.AddDynamic(this, &UVDTitlePanelUserWidget::OnClickExitButton);
 	}
 
