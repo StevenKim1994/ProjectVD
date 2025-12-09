8f298bec2d5f55deb7b710b7e340086d0fd55685
김시윤 <enqueuedequeue@naver.com>
2025-12-09 13:16:21 +0900

    방향감 개선

--- Source/ProjectVD/Animation/VDAnimInstance.cpp
+++ Source/ProjectVD/Animation/VDAnimInstance.cpp
@@ -9,7 +9,6 @@
 UVDAnimInstance::UVDAnimInstance()
 {
 	MovingThreshold = 3.f;
-	JumpingThreshold = 100.f;
 }
 
 void UVDAnimInstance::SetIsLockOnTarget(uint8 bIsLockOn)
@@ -41,34 +40,25 @@ void UVDAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 	{
 		if (bIsLockOnTarget)
 		{
-			Velocity = OwnerCharacterMovement->Velocity;
-			GroundSpeed = Velocity.Size2D();
-			Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, OwnerCharacter->GetActorRotation());
+			const FVector Vel = OwnerCharacterMovement->Velocity;
+			const float FS = FVector::DotProduct(OwnerCharacter->GetActorForwardVector(), Vel);
+			const float RS = FVector::DotProduct(OwnerCharacter->GetActorRightVector(), Vel);
 
-			const FVector Forward = OwnerCharacter->GetActorForwardVector();
-			const FVector Vel2D(Velocity.X, Velocity.Y, 0.f);
-			const float Dot = FVector::DotProduct(Forward, Vel2D.GetSafeNormal());
+			ForwardSpeed = FS;
+			RightSpeed = RS;
 
-			// 반대 방향(뒷걸음질) 입력 시 음수 방향으로 처리
-			Direction = (Dot < 0.f) ? -FMath::Abs(Direction) : FMath::Abs(Direction);
-
-			// Direction이 음수일 시 GroundSpeed도 음수 처리
-			GroundSpeed = (Direction < 0.f) ? -FMath::Abs(GroundSpeed) : FMath::Abs(GroundSpeed);
-
-			bIsIdle = GroundSpeed < MovingThreshold && (bIsDead == false);
-			bIsFalling = OwnerCharacterMovement->IsFalling();
-			bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold);
-			UE_LOG(LogTemp, Log, TEXT("UVDAnimInstance::NativeUpdateAnimation() GroundSpeed: %f, Direction: %f, bIsIdle: %d, bIsFalling: %d, bIsJumping: %d"),
-			GroundSpeed, Direction, bIsIdle, bIsFalling, bIsJumping);
+			bIsIdle = (ForwardSpeed == 0 && RightSpeed == 0) && (bIsDead == false);
 		}
 		else
 		{
-			Velocity = OwnerCharacterMovement->Velocity;
-			GroundSpeed = Velocity.Size2D();
-			Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, OwnerCharacter->GetActorRotation());
-			bIsIdle = GroundSpeed < MovingThreshold && (bIsDead == false);
-			bIsFalling = OwnerCharacterMovement->IsFalling();
-			bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold);
+			const FVector Vel = OwnerCharacterMovement->Velocity;
+			const float FS = FVector::DotProduct(OwnerCharacter->GetActorForwardVector(), Vel);
+			const float RS = FVector::DotProduct(OwnerCharacter->GetActorRightVector(), Vel);
+			
+			ForwardSpeed = FS;
+			RightSpeed = RS;
+
+			bIsIdle = (ForwardSpeed == 0 && RightSpeed == 0) && (bIsDead == false);
 		}
 	}
 }
--- Source/ProjectVD/Animation/VDAnimInstance.h
+++ Source/ProjectVD/Animation/VDAnimInstance.h
@@ -31,32 +31,20 @@ protected:
 	TObjectPtr<UCharacterMovementComponent> OwnerCharacterMovement;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	FVector Velocity;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	float GroundSpeed;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	float Direction;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
 	uint8 bIsIdle : 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	uint8 bIsFalling : 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	uint8 bIsJumping : 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
 	float MovingThreshold;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
-	float JumpingThreshold;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat");
 	uint8 bIsLockOnTarget : 1;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character");
 	uint8 bIsDead : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
+	float ForwardSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character");
+	float RightSpeed;
 };
